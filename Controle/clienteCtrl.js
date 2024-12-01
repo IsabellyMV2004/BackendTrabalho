83d2361167562366d2d50eb80362ad8d089ac8ce
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2024-12-01 14:39:21 -0300

    clientes

--- Controle/clienteCtrl.js
+++ Controle/clienteCtrl.js
@@ -1,186 +1,192 @@
 //É a classe responsável por traduzir requisições HTTP e produzir respostas HTTP
 import Cliente from "../Modelo/cliente.js";
 
-export default class ClienteCtrl{
+export default class ClienteCtrl {
 
-    gravar(requisicao, resposta){
+    gravar(requisicao, resposta) {
         //preparar o destinatário que a resposta estará no formato JSON
         resposta.type("application/json");
         //Verificando se o método da requisição é POST e conteúdo é JSON
-        if (requisicao.method == 'POST' && requisicao.is("application/json")){
-            const email  = requisicao.body.email;
-            //pseudo validação
-            if (email)
-            {
-                //gravar a cliente
-                const cliente = new Cliente(0,email);
-                cliente.gravar()
-                .then(()=>{
-                    resposta.status(200).json({
-                        "status":true,
-                        "mensagem":"Cliente adicionada com sucesso!",
-                        "codigo": cliente.codigo
+        if (requisicao.method == 'POST' && requisicao.is("application/json")) {
+            const nome = requisicao.body.nome;
+            const endereco = requisicao.body.endereco;
+            const telefone = requisicao.body.telefone;
+            const email = requisicao.body.email;
+
+            if (nome && endereco  &&
+                telefone && email) {
+                //gravar o cliente
+
+                const cliente = new Cliente(0,
+                    nome, endereco, telefone,
+                    email);
+
+                cliente.incluir()
+                    .then(() => {
+                        resposta.status(200).json({
+                            "status": true,
+                            "mensagem": "Cliente adicionado com sucesso!",
+                            "codigo": cliente.codigo
+                        });
+                    })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Não foi possível incluir o cliente: " + erro.message
+                        });
                     });
-                })
-                .catch((erro)=>{
-                    resposta.status(500).json({
-                        "status":false,
-                        "mensagem":"Não foi possível incluir a cliente: " + erro.message
-                    });
-                });
             }
-            else
-            {
+            else {
                 resposta.status(400).json(
                     {
-                        "status":false,
-                        "mensagem":"Informe corretamente todos os dados de uma cliente conforme documentação da API."
+                        "status": false,
+                        "mensagem": "Informe corretamente todos os dados de um cliente conforme documentação da API."
                     }
                 );
             }
-
         }
-        else
-        {
+        else {
             resposta.status(400).json({
-                "status":false,
-                "mensagem":"Requisição inválida! Consulte a documentação da API."
+                "status": false,
+                "mensagem": "Requisição inválida! Consulte a documentação da API."
             });
 
         }
 
     }
 
-    editar(requisicao, resposta){
+    editar(requisicao, resposta) {
         //preparar o destinatário que a resposta estará no formato JSON
         resposta.type("application/json");
         //Verificando se o método da requisição é POST e conteúdo é JSON
-        if ((requisicao.method == 'PUT' || requisicao.method == 'PATCH') && requisicao.is("application/json")){
+        if ((requisicao.method == 'PUT' || requisicao.method == 'PATCH') && requisicao.is("application/json")) {
             //o código será extraída da URL (padrão REST)
             const codigo = requisicao.params.codigo;
-            const email  = requisicao.body.email;
-            
-            //pseudo validação
-            if (codigo > 0 && email)
-            {
-                //alterar a cliente
-                const cliente = new Cliente(codigo,email);
-                cliente.editar().then(()=>{
-                    resposta.status(200).json({
-                        "status":true,
-                        "mensagem":"cliente alterada com sucesso!",
+            const nome = requisicao.body.nome;
+            const endereco = requisicao.body.endereco;
+            const telefone = requisicao.body.telefone;
+            const email = requisicao.body.email;
+
+            if (codigo > 0 && nome && endereco  &&
+                telefone && email) {
+                //gravar o cliente
+
+                const cliente = new Cliente(codigo,
+                    nome, endereco, telefone,
+                    email);
+
+                cliente.alterar()
+                    .then(() => {
+                        resposta.status(200).json({
+                            "status": true,
+                            "mensagem": "Cliente adicionado com sucesso!",
+                            "codigo": cliente.codigo
+                        });
+                    })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Não foi possível incluir o cliente: " + erro.message
+                        });
                     });
-                })
-                .catch((erro)=>{
-                    resposta.status(500).json({
-                        "status":false,
-                        "mensagem":"Não foi possível alterar a cliente: " + erro.message
-                    });
-                });
             }
-            else
-            {
+            else {
                 resposta.status(400).json(
                     {
-                        "status":false,
-                        "mensagem":"Informe corretamente todos os dados de uma cliente conforme documentação da API."
+                        "status": false,
+                        "mensagem": "Informe corretamente todos os dados de um cliente conforme documentação da API."
                     }
                 );
             }
         }
-        else
-        {
+        else {
             resposta.status(400).json({
-                "status":false,
-                "mensagem":"Requisição inválida! Consulte a documentação da API."
+                "status": false,
+                "mensagem": "Requisição inválida! Consulte a documentação da API."
             });
 
         }
     }
 
-    excluir(requisicao, resposta){
+    excluir(requisicao, resposta) {
         //preparar o destinatário que a resposta estará no formato JSON
         resposta.type("application/json");
         //Verificando se o método da requisição é POST e conteúdo é JSON
-        if (requisicao.method == 'DELETE'){
+        if (requisicao.method == 'DELETE') {
             //o código será extraída da URL (padrão REST)
             const codigo = requisicao.params.codigo;
             //pseudo validação
-            if (codigo > 0)
-            {
+            if (codigo > 0) {
                 //alterar o cliente
                 const cliente = new Cliente(codigo);
                 cliente.excluir()
-                .then(()=>{
-                    resposta.status(200).json({
-                        "status":true,
-                        "mensagem":"cliente excluída com sucesso!",
+                    .then(() => {
+                        resposta.status(200).json({
+                            "status": true,
+                            "mensagem": "Cliente excluído com sucesso!",
+                        });
+                    })
+                    .catch((erro) => {
+                        resposta.status(500).json({
+                            "status": false,
+                            "mensagem": "Não foi possível excluir o cliente: " + erro.message
+                        });
                     });
-                })
-                .catch((erro)=>{
-                    resposta.status(500).json({
-                        "status":false,
-                        "mensagem":"Não foi possível excluir o cliente: " + erro.message
-                    });
-                });
             }
-            else
-            {
+            else {
                 resposta.status(400).json(
                     {
-                        "status":false,
-                        "mensagem":"Informe um código válido de uma cliente conforme documentação da API."
+                        "status": false,
+                        "mensagem": "Informe um código válido de um cliente conforme documentação da API."
                     }
                 );
             }
 
         }
-        else
-        {
+        else {
             resposta.status(400).json({
-                "status":false,
-                "mensagem":"Requisição inválida! Consulte a documentação da API."
+                "status": false,
+                "mensagem": "Requisição inválida! Consulte a documentação da API."
             });
 
         }
     }
-
-    consultar(requisicao, resposta){
+    
+    consultar(requisicao, resposta) {
         resposta.type("application/json");
-        if (requisicao.method=="GET"){
+        if (requisicao.method == "GET") {
             let codigo = requisicao.params.codigo;
             //evitar que código tenha valor undefined
-            if (isNaN(codigo)){
+            if (isNaN(codigo)) {
                 codigo = "";
             }
 
             const cliente = new Cliente();
             //método consultar retorna uma lista de clientes
             cliente.consultar(codigo)
-            .then((listaClientes) =>{
-                resposta.status(200).json(listaClientes
-                    /*{
-                        "status": true,
-                        "listaclientes": listaclientes
-                    }*/
-                );
-            })
-            .catch((erro) => {
-                resposta.status(500).json(
-                    {
-                        "status":false,
-                        "mensagem":"Erro ao consultar clientes:" + erro.message    
-                    }
-                );
-            });
+                .then((listaClientes) => {
+                    resposta.status(200).json(listaClientes
+                        /*{
+                            "status": true,
+                            "listaClientes": listaClientes
+                        }*/
+                    );
+                })
+                .catch((erro) => {
+                    resposta.status(500).json(
+                        {
+                            "status": false,
+                            "mensagem": "Erro ao consultar clientes: " + erro.message
+                        }
+                    );
+                });
 
         }
-        else
-        {
+        else {
             resposta.status(400).json(
                 {
-                    "status":false,
-                    "mensagem":"Requisição inválida! Consulte a documentação da API."
+                    "status": false,
+                    "mensagem": "Requisição inválida! Consulte a documentação da API."
                 }
             );
         }
